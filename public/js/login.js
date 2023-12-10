c5dba06b86ead9f2587c405ded238cec5b7bceeb
Nicolas Figueroa <n.figueroa.diaz21gmail.com>
2023-12-10 20:36:14 -0300

    add error

--- public/js/login.js
+++ public/js/login.js
@@ -6,22 +6,31 @@ btnLogIn.addEventListener("click", async () => {
   const valueUsername = username.value;
   const valuePassword = password.value;
   const urlApiLogin = "https://brasas-colchagua.onrender.com/login";
-  const res = await fetch(urlApiLogin, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: valueUsername,
-      password: valuePassword,
-    }),
-  });
-  const response = await res.json();
-  const { login, token } = response;
-  if (login) {
-    localStorage.setItem("token", token);
-    window.location.assign("/");
-  } else {
-    alert("Error al iniciar sesion");
+
+  try {
+    const res = await fetch(urlApiLogin, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: valueUsername,
+        password: valuePassword,
+      }),
+    });
+
+    const response = await res.json();
+    const { login, token } = response;
+
+    if (login) {
+      localStorage.setItem("token", token);
+      window.location.assign("/");
+    } else {
+      alert("Error al iniciar sesión");
+    }
+  } catch (error) {
+    console.error("Error durante la operación fetch:", error);
+    // Manejar el error apropiadamente (por ejemplo, mostrar un mensaje de error al usuario)
+    alert("Error durante la operación de inicio de sesión. Por favor, inténtalo de nuevo.");
   }
 });
